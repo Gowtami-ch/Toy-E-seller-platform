1b3640dd59572c82107e907829ed5ff0d0f359e7
iamgowtami <cs19b010@iittp.ac.in>
2022-04-04 02:28:10 +0530

    sellers home page done

--- E-Seller/src/Components/sellersHome/Sellers.js
+++ E-Seller/src/Components/sellersHome/Sellers.js
@@ -1,14 +1,13 @@
 import React from "react";
-// import { Header } from "./Header";
+import { Header } from "./header";
 import styles from "./sellershome.module.css";
 import Sellershome from "./Sellershome";
 export const Sellers = () => {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <div className="container-fluid">
         <div className="row flex-nowrap">
-          {/* <Navbar /> */}
           <div className="col py-3">
             <Sellershome />
           </div>
